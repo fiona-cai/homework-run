0956ccd73f4a456b7f9533e36358f74556e409d3
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2024-08-17 12:04:46 -0400

    mediapipe pose working

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
 import cv2
+import mediapipe as mp
+import lights
+import time
 vid = cv2.VideoCapture(0) 
 vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+pose_detector = mp.solutions.pose.Pose(static_image_mode=True)
+light_port = lights.get_port()
+
+time.sleep(2) # wait for the lights to connect
+
+connections = [
+    (16, 14),
+    (14, 12),
+    (12, 11),
+    (12, 24),
+    (24, 23),
+    (11, 23),
+    (11, 13),
+    (13, 15),
+    (24, 26),
+    (26, 28),
+    (23, 25),
+    (25, 27)
+]
+
+lights.left_on(light_port)
+lights.middle_on(light_port)
+lights.right_on(light_port)
 while(True): 
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read()
+    results = pose_detector.process(frame)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        x_diff = int(results.pose_landmarks.landmark[7].x * frame.shape[1]) - int(results.pose_landmarks.landmark[8].x * frame.shape[1])
+        y_diff = int(results.pose_landmarks.landmark[7].y * frame.shape[0]) - int(results.pose_landmarks.landmark[8].y * frame.shape[0])
+        square = x_diff ** 2 + y_diff ** 2
+        distance = int(square ** 0.5)
+        head_point = (int(results.pose_landmarks.landmark[0].x * frame.shape[1]), int(results.pose_landmarks.landmark[0].y * frame.shape[0]))
+        cv2.circle(frame, head_point, distance, (0, 255, 0), 3)
+        for connection in connections:
+            start_idx, end_idx = connection
+            start_landmark = results.pose_landmarks.landmark[start_idx]
+            end_landmark = results.pose_landmarks.landmark[end_idx]
+            start_point = (int(start_landmark.x * frame.shape[1]), int(start_landmark.y * frame.shape[0]))
+            end_point = (int(end_landmark.x * frame.shape[1]), int(end_landmark.y * frame.shape[0]))
+            cv2.circle(frame, start_point, 5, (0, 0, 255), -1)
+            cv2.circle(frame, end_point, 5, (0, 0, 255), -1)
+            cv2.line(frame, start_point, end_point, (255, 0, 0), 2)
+
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
@@ -16,3 +61,4 @@ while(True):
 vid.release() 
 # Destroy all the windows 
 cv2.destroyAllWindows() 
+lights.close(light_port)
