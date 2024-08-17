781aa873e0ed65363a2fd03f61f2f5ab4ebd947a
Serena X <84420380+xuserena12@users.noreply.github.com>
2024-08-17 10:51:28 -0400

    added video capture

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+vid = cv2.VideoCapture(0) 
+vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while(True): 
+      
+    # Capture the video frame 
+    # by frame 
+    ret, frame = vid.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+
+
+# After the loop release the cap object 
+vid.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
